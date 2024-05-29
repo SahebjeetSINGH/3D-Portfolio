eb73bd4c233808d33df0485b59dbb6522fa630f9
Sahebjeet singh <sahebjeet2001@gmail.com>
2024-05-29 22:50:54 +0530

    made skeleton of website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navlinks } from "../con";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div>Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
